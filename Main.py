d15ff25fd7b6cf674c6106bfafda0f4f604c6106
Ebin Alex <ebinalex@karunya.edu.in>
2022-08-10 10:10:29 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,21 +1,6 @@
 class Solution:
-    """This class implements linear queue.
-      Attributes:
-          stack: A list which maintains the content of stack.
-          queue: A list which maintains the content of queue.
-          top: An integer which denotes the index of the element at the top of the stack.
-          front: An integer which denotes the index of the element at the front of the queue.
-          rear: An integer which denotes the index of the element at the rear of the queue.
-          size: An integer which represents the size of stack and queue.
-      """
-
     # Write your code here
     def _init_(self, size):
-        
-        """Inits Solution with stack, queue, size, top, front and rear.
-        Arguments:
-          size: An integer to set the size of stack and queue.
-        """
         self.stack = [None]*size
         self.queue = [None]*size
         self.size = size
@@ -24,28 +9,12 @@ class Solution:
         self.front = -1
 
     def is_stack_empty(self):
-        """
-        Check whether the stack is empty.
-        Returns:
-          True if it is empty, else returns False.
-        """
-        
         return self.top==-1
 
     def is_queue_empty(self):
-        """
-        Check whether the queue is empty.
-        Returns:
-          True if it is empty, else returns False.
-        """
-        return self.rear<self.front
+        return self.rear>self.front
 
     def is_stack_full(self):
-        """
-        Check whether the stack is full.
-        Returns:
-          True if it is full, else returns False.
-        """
         return self.top==(self.size-1)
 
 
